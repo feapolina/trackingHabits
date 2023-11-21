6417c6f2a29e1782022bd860bef65345378e11cb
Felipe Apolinário <fe.cavalcanti2016@gmail.com>
2023-11-21 17:51:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+/* tudo dentro dessa função será executado ao carregar a página */
 document.addEventListener("DOMContentLoaded", function () {
   const contributionGraph = document.getElementById("contribution-graph");
 
+  /* gerando a tabela de registros */
   const contributions = Array.from({ length: 365 });
 
   contributions.forEach((contribution) => {
     const square = document.createElement("div");
     square.classList.add("contribution-square");
-
+    
     if (contribution) {
       square.classList.add("contribution-green");
     } else {
       square.classList.add("contribution-gray");
     }
 
+    // interatividade com as caixinhas
     square.addEventListener("click", function () {
-      // Simulação de interatividade (pode ser personalizado conforme necessário)
       if (contribution) {
         square.classList.remove("contribution-green");
         square.classList.add("contribution-gray");
